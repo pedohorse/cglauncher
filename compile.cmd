c2eb0a01b008984c598f3d131d2a8d9f62cd22c2
User <User>
2017-09-23 00:05:39 +0200

    launcher dist in zip

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,3 @@
-c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json --base-name WIN32GUI main.py
-xcopy launcherPanes .\dist\. /Y
+c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json --target-dir=dist\\cglauncher --base-name WIN32GUI main.py
+xcopy launcherPanes .\dist\cglauncher\. /Y
+#TODO: make a py script that does it! fucking CMD is fucking fuck
