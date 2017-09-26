913df786f818505c73eed7eec14ac590b3dcc0f4
User <User>
2017-09-26 23:17:31 +0200

    compile target file changed

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,3 @@
-c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json --target-dir=dist\\cglauncher --icon=icon.ico --base-name WIN32GUI main.py
+c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json --target-dir=dist\\cglauncher --icon=icon.ico --base-name=WIN32GUI --target-name=cglauncher.exe main.py
 xcopy launcherPanes dist\cglauncher\launcherPanes /E /Y
 c:\\python27\\python.exe postsetup.py
