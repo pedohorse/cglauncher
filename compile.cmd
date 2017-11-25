da951e16f5491c99c4e0c1f2f7b6f3bf5b9978c6
pedohorse <xapkohheh@gmail.com>
2017-11-25 14:41:30 +0100

    compiled version

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
-c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json --target-dir=dist\\cglauncher --base-name=WIN32GUI --icon=icon.ico --target-name=cglauncher.exe main.py
+c:\\python27\\python.exe C:\\Python27\\Scripts\\cxfreeze --include-modules=json,platform --target-dir=dist\\cglauncher --base-name=WIN32GUI --icon=icon.ico --target-name=cglauncher.exe main.py
 #
 xcopy launcherPanes dist\cglauncher\launcherPanes /E /Y
 c:\\python27\\python.exe postsetup.py
